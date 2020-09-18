0adf03b50476ebd7351d6fa2d1dfa99f58955f43
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-09-18 22:45:52 +0300

    Add 01.Fruit

---
+++ JavaScript/SYNTAX, FUNCTIONS AND STATEMENTS/Exercise/01. Fruit/P01. Fruit.js
@@ -0,0 +1,7 @@
+function fruit(typeOfFruit, weightInGrams, pricePerKg) {
+    let weightInKg = (weightInGrams / 1000);
+    let totalPrice = (weightInKg * pricePerKg);
+    console.log(`I need $${totalPrice.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${typeOfFruit}.`);        
+}
+
+fruit('orange', 2500, 1.80);
