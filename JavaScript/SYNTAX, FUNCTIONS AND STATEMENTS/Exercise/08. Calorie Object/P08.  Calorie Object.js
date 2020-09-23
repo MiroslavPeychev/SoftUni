8899cde79eb32aec48379d745144516ee493a2e1
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-09-23 14:01:41 +0300

    Add Calorie Object

---
+++ JavaScript/SYNTAX, FUNCTIONS AND STATEMENTS/Exercise/08. Calorie Object/P08.  Calorie Object.js
@@ -0,0 +1,14 @@
+function takeFoodCalories(inputArr) {
+    let foodArgs = [];
+
+    for (let i = 0; i < inputArr.length; i += 2) {
+        let foodName = inputArr[i];
+        let foodCalories = inputArr[i + 1];
+
+        foodArgs.push(`${foodName}: ${foodCalories}`);
+    }
+
+    console.log(`{ ${foodArgs.join(', ')} }`);
+}
+
+takeFoodCalories(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
