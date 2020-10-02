8c04e402d6523f835464654a5853305ae2d10881
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-10-02 23:33:48 +0300

    Add P11. Spiral Matrix

---
+++ JavaScript/Arrays/Exercise/11. Spiral Matrix/P11. Spiral Matrix.js
@@ -0,0 +1,45 @@
+function fillSpiralMatrix(rows, cols) {
+    
+    let result = new Array(rows).map(() => new Array(cols).fill(''));
+    let counter = 1;
+    let startCol = 0;
+    let endCol = cols - 1;
+    let startRow = 0;
+    let endRow = rows - 1;
+
+    while(startCol <= endCol && startRow <= endRow) {
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter;
+            counter++;
+        }
+
+        startRow++;
+
+        for (let j = startRow; j <= endRow; j++) {
+            result[j][endCol] = counter;
+            counter++;
+        }
+
+        endCol--;
+
+        for (let k = endCol; k >= startCol; k--) {
+            result[endRow][i] = counter;
+            counter++;
+        }
+
+        endRow--;
+
+        for (let m = endRow; m >= startRow; m--) {
+            result[i][startCol] = counter;
+            counter++;
+        }
+
+        startCol++;
+    }
+
+    for(let row in result) {
+        console.log(result[row].join(" "));
+    }
+}
+
+fillSpiralMatrix(5, 5);
