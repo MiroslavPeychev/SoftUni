491fa1b9a5ae31b9e0c4a43f24ea0d016ca0245d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-10-06 23:07:40 +0300

    Add P02.Fibonacci

---
+++ JavaScript/Function Context/Exercise/02. Fibonacci/P02. Fibonacci.js
@@ -0,0 +1,25 @@
+function getFibonator() {
+    let currentNum = 0;
+    let nextNum = 1;
+
+    return () => {
+        let fibNum = currentNum + nextNum;
+        currentNum = nextNum;
+        nextNum = fibNum;
+
+        return currentNum;
+    }
+}
+
+function solve() {
+    let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
+}
+
+solve();
