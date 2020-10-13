c4f9de322c8155b8809be15debf68b060ed64b2c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-10-13 23:02:39 +0300

    Add P01.Heroes

---
+++ JavaScript/Object Composition/LAB/01. Heroes/P01. Heroes.js
@@ -0,0 +1,31 @@
+solve = () => {
+    let fighter = function(name = '') {
+        [this. name, this.health, this.stamina] = [name,100,100];
+        
+        this.fight = () => {
+            this.stamina--;
+            console.log(`${this.name} slashes at the foe!`);
+        }
+    }
+
+    const mage = function (name = '') {
+        [this. name, this.health, this.mana] = [name,100,100];
+
+        this.cast = (spell) => {
+            this.mana--;
+            console.log(`${this.name} cast ${spell}`);
+        }
+    }
+
+    return {
+        fighter: (name = '') =>  new fighter(name),
+        mage: (name = '') => new mage(name)
+    };
+}
+
+let create = solve();
+let warrior = create.fighter('Gosho');
+warrior.fight();
+
+let mage = create.mage()
+mage.cast('Frost nova');
