2493fe84b91323567b4c5c226f144df0bfc63b7f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-10-14 17:31:31 +0300

    Modified - use Map() to get first entered city with the same price.

--- JavaScript/Objects & Classes/LAB/05. Lowest Prices in Cities/P05. Lowest Prices in Cities.js
+++ JavaScript/Objects & Classes/LAB/05. Lowest Prices in Cities/P05. Lowest Prices in Cities.js
@@ -1,27 +1,31 @@
-function findLowestPriceIncities(input) {
-    let products = {};
-    let towns = [];
+function findLowestPriceIncities(inputArr) {
+    let products = new Map();
 
-    input.forEach(row => {
-        let [town, productName, priceAsText] = row.split(' | ');
-        let price = Number(priceAsText);
-        towns.push(town);
-        let tempTown = town;
-
-
-        if (!products[productName] || products[productName].price > price || products[productName].town == town) {
-            products[productName] = {
-                
-                
-                town: town,
-                price: price,
-            };
+    inputArr.forEach(row => {
+        let [town, productName, price] = row.split(' | ');
+        if (!products.has(productName)) {
+            products.set(productName, new Map());
         }
-    });
 
-    Object.keys(products).forEach(product =>
-        console.log(`${product} -> ${products[product].price} (${products[product].town})`)
-    );
+        products.get(productName).set(town, Number(price));
+
+
+        
+    })
+
+    for (let [key, value] of products) {
+           let lowest = ([...value].reduce(function (a, b) {
+            if (a[1] < b[1]) {
+                return a;
+            } else if (a[1] > b[1]) {
+                return b;
+            }
+
+            return a;
+           }));
+
+           console.log(`${key} -> ${lowest[1]} (${lowest[0]})`);
+    }
 }
 
 findLowestPriceIncities(['Sample Town | Sample Product | 1000',
@@ -34,71 +38,3 @@ findLowestPriceIncities(['Sample Town | Sample Product | 1000',
 );
 
 
-function solve() {
-    const recipes = {
-      apple: {
-        carbohydrate: 1,
-        flavour: 2
-      }, 
-      lemonade: {
-        carbohydrate: 10,
-        flavour: 20
-      },
-      burger: {
-        carbohydrate: 5,
-        fat: 7,
-        flavour: 3
-      },
-      eggs: {
-        protein: 5,
-        fat:1,
-        flavour: 1
-      },
-      turkey: {
-        protein:10,
-        fat: 10,
-        carbohydrates: 10,
-        flavour: 10,
-      }
-    }
-   
-    const stocks = {
-      protein: 0,
-      carbohydrate: 0,
-      fat: 0,
-      flavour: 0
-    };
-   
-    const commands = {
-      restock : (microelement, quantity) =>  {
-        stocks[microelement] += quantity
-        return 'Success';
-      },
-      prepare: (product, quanity) => { 
-        let recipe = Object.entries(recipes[product]);
-   
-        for(let[item,countNeeded] of recipe) {
-            if(stocks[item] < countNeeded * quanity) {
-              return `Error: not enough ${item} in stock`;
-            }
-        }
-   
-        recipe.forEach(([item, countNeeded]) => {
-          stocks[item] -= countNeeded * quanity;
-        });
-   
-        return 'Success'
-   
-      },
-      report : () => Object.
-                      entries(stocks).
-                      map(([microelement, count]) => `${microelement}=${count}` ).
-                      join(' ')
-   
-    };
-   
-    return (input) => {
-      let [command, item, count] = input.split(' ');
-      return commands[command](item, +count);
-    }
-  }
