885ccb2b0d68bc5914f9eacfe6d4eb65807a18d9
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-11-03 08:17:59 +0200

    Add 03.Growing Word

---
+++ JavaScript/DOM/LAB/03. Growing Word/solution.js
@@ -0,0 +1,28 @@
+function growingWord() {
+
+  let p = document.querySelector('#exercise p');
+  let currentSizeInPx = p.style.fontSize.slice(0, -2) * 2 || 2;
+  p.style.fontSize = `${currentSizeInPx}px`;
+
+  let blueDiv = document.getElementById('blueDiv');
+  let greenDiv = document.getElementById('greenDiv');
+  let redDiv = document.getElementById('redDiv');
+
+  let isBlue = blueDiv.getAttribute('active') === "true";
+  let isGreen = greenDiv.getAttribute('active') === "true";
+  let isRed = redDiv.getAttribute('active') === "true";
+
+  if (currentSizeInPx === 2 || isBlue) {
+    p.style.color = "blue";
+    blueDiv.setAttribute("active", false);
+    greenDiv.setAttribute("active", true);
+  } else if (isGreen) {
+    p.style.color = "green";
+    greenDiv.setAttribute("active", false);
+    redDiv.setAttribute("active", true);
+  } else if (isRed) {
+    p.style.color = "red";
+    redDiv.setAttribute("active", false);
+    blueDiv.setAttribute("active", true);
+  }
+}
