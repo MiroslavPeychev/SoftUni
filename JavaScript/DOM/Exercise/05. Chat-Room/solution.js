0b62a04fe20a39c28cab4084ffa83db25a0bd7d7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-11-10 23:47:54 +0200

    Add 05.Chat-room

---
+++ JavaScript/DOM/Exercise/05. Chat-Room/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+   let inputAsText = document.getElementById('chat_input');
+   let sendButton = document.getElementById('send');
+   let myMessagesTemplate = document.getElementsByClassName('my-message')[0];
+   let placeToDisplayMsg = document.getElementById('chat_messages');
+
+   sendButton.addEventListener('click', function () {
+      let div = myMessagesTemplate.cloneNode(true);
+      let message = inputAsText.nodeValue;
+
+      let isTyping = document.getElementById("typing");
+
+      if (message !== '') {
+         div.textContent = message;
+         placeToDisplayMsg.appendChild(div);
+         inputAsText.value = '';
+      }
+   });
+}
