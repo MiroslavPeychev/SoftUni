ba4198f821a04174e1003f518d7497c9b3fd9ed0
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-12-05 22:41:03 +0200

    Add 06.Table-Search-Engine

---
+++ JavaScript/DOM Manipulations/Exercise DOM Manipulations/06. Table-Search-Engine/solution.js
@@ -0,0 +1,20 @@
+function solve() {
+   let searchInputElement = document.getElementById('searchField');
+   let searchButton = document.getElementById('searchBtn');
+   let allRowsElements = document.querySelectorAll('tbody > tr');
+
+   searchButton.addEventListener('click', () => {
+      let searchInput = searchInputElement.value;
+      let regex = new RegExp(searchInput, 'gim');
+
+      Array.from(allRowsElements.map(currentTableRow => {
+         currentTableRow.classList.remove('select');
+
+         if (currentTableRow.textContent.match(regex) !== null) {
+            currentTableRow.classList.add('select');
+         }
+      }));
+
+      searchInputElement.value = '';
+   });
+}
