7da42d93aa6347d8ed679b72b4fbc11680251d29
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-12-19 23:41:08 +0200

    Modified

--- JavaScript/DOM Manipulations/Exercise DOM Manipulations/08. Cards/solution.js
+++ JavaScript/DOM Manipulations/Exercise DOM Manipulations/08. Cards/solution.js
@@ -1,3 +1,52 @@
 function solve() {
-   // TODO...
+   const playerFirst = document.getElementById('player1Div');
+   const playerSecond = document.getElementById('player2Div');
+   const result = document.getElementById('result').children;
+   const history = document.getElementById('history');
+
+   let playerFirstCard = '';
+   let playerSecondCard = '';
+
+   [playerFirst, playerSecond].forEach(player => player.addEventListener('click', (e) => {
+      if (player === playerFirst) {
+         playerFirstCard = playerFirstCard = showCard(playerFirstCard, result[0], e);
+      } else {
+         playerSecondCard = playerSecondCard = showCard(playerSecondCard, result[2], e);
+      }
+
+      cardBattle();
+   }));
+
+   function cardBattle() {
+      let leftSpan = result[0].textContent;
+      let rightSpan = result[2].textContent;
+
+      if (leftSpan !== '' && rightSpan !== '') {
+         let resultFirstPlayer = Number(playerFirstCard.name);
+         let resultSecondPlayer = Number(playerSecondCard.name);
+
+         if (resultFirstPlayer > resultSecondPlayer) {
+            createBorder(playerFirstCard, playerSecondCard)
+         } else {
+            createBorder(playerSecondCard, playerFirstCard)
+         }
+
+         saveHistory();
+         defaultValues();
+      }
+   }
+
+   function createBorder(card1, card2) {
+      card1.style.border = "2px solid green";
+      card2.style.border = "2px solid red";
+   }
+
+   function showCard(player, span, e) {
+      e.target.src = "images/whiteCard.jpg";
+      span.textContent = e.target.name;
+      player = e.target;
+      return player;
+   }
+
+   function
 }
